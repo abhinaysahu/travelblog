fb84627db3ded7039608ac4cf61488022babf467
abhinaysahu <abhin4ysahu@gmail.com>
2024-11-04 23:09:38 +0530

    google map API script updated

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 // Load Navbar
-fetch('/partials/navbar.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('navbar').innerHTML = data;
+fetch("/partials/navbar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("navbar").innerHTML = data;
   })
-  .catch(error => console.error('Error loading navbar:', error));
+  .catch((error) => console.error("Error loading navbar:", error));
 
 // Load Footer
-fetch('/partials/footer.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('footer').innerHTML = data;
+fetch("/partials/footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
   })
-  .catch(error => console.error('Error loading footer:', error));
+  .catch((error) => console.error("Error loading footer:", error));
+
+const script = document.createElement("script");
+script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
+script.async = true;
+document.head.appendChild(script);
